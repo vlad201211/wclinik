9df52e489c12eda2e971afc12355bae6b9764c9b
qwqwqwqw <qwqwqwqw@vlad>
2015-01-19 11:22:31 +0300

    20150119-4

--- wp-content/plugins/widgetclinik/widgetclinik.php
+++ wp-content/plugins/widgetclinik/widgetclinik.php
@@ -8,7 +8,7 @@ Author: dxlab
 Author URI: dxlab.ru
 */
 
-// Выводит форму в админке 112311
+// Выводит форму в админке
 function get_wclinik_form() {
 ?>
 	<div class="wrap">
@@ -35,7 +35,54 @@ function wclinik_world($title) {
 	echo $title.' -> '.get_option('wclinik_1c_url');
 }
 
+// Вешаем хук
 add_filter('the_title', 'wclinik_world');
+// Добавляем в меню пункт
 add_action('admin_menu', 'wclinik_admin_menu');
+
+
+
+// Иницилизация
+global $jal_db_version;
+$jal_db_version = "1.0";
+
+function jal_install () {
+   global $wpdb;
+   global $jal_db_version;
+
+   // Имя тестовой таблицы
+   $table_name = $wpdb->prefix . "liveshoutbox";
+   // Проверяем если таблицы реально нет, тогда создаем
+   if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+      
+      $sql = "CREATE TABLE " . $table_name . " (
+	  id mediumint(9) NOT NULL AUTO_INCREMENT,
+	  time bigint(11) DEFAULT '0' NOT NULL,
+	  name tinytext NOT NULL,
+	  text text NOT NULL,
+	  url VARCHAR(55) NOT NULL,
+	  UNIQUE KEY id (id)
+	);";
+      
+      // Подключаем файл для работы с базой
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+      // Выполняем запрос
+      dbDelta($sql);
+      
+      // Информация для теста
+      $welcome_name = "Mr. Wordpress";
+      $welcome_text = "Поздравляю, установка прошла успешно!";
+      
+      // Вносим данные в таблицу
+      $rows_affected = $wpdb->insert( $table_name, array( 'time' => current_time('mysql'), 'name' => $welcome_name, 'text' => $welcome_text ) );
+      
+      // Записываем версию
+      add_option("jal_db_version", $jal_db_version);
+
+   }
+}
+
+// Регистрируем версию
+register_activation_hook(__FILE__,'jal_install');
 
 ?>
